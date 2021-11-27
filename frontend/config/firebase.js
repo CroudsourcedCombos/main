e7782da871202cc1ccaf2ab5a13974bab65a075e
Raj Piskala <altoraries@gmail.com>
2021-11-27 14:53:51 -0800

    Rename .env and add more keys to firebase config

--- frontend/config/firebase.js
+++ frontend/config/firebase.js
@@ -1,15 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import dotenv from 'dotenv'
-dotenv.config()
+// import dotenv from 'dotenv'
+// dotenv.config()
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  appId: process.env.APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
