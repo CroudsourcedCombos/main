2a756f9d87c627a91d56d3e18411421693c71204
Raj Kumar <raj.a.kumar@ucla.edu>
2021-11-26 22:24:06 -0800

    Added multiline text field and added Review star picker

--- frontend/pages/add-review.js
+++ frontend/pages/add-review.js
@@ -1,11 +1,43 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import * as React from "react";
+import styles from "../styles/Home.module.css";
+import TextField from "@mui/material/TextField";
+import Rating from "@mui/material/Rating";
 
 export default function AddReview() {
+  const [value, setValue] = React.useState(2);
+  const [reviewText, setreviewText] = React.useState("Controlled");
+
+  const handleChange = (event) => {
+    setValue(event.target.reviewText);
+  };
+
   return (
-    <div className={styles.container}>
-      <h1>Hello World</h1>
-    </div>
-  )
+    <>
+      <div className={styles.container}>
+        <h1>Add Review</h1>
+      </div>
+      <div>
+        <Rating
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
+      </div>
+      <div>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Multiline"
+          multiline
+          minRows={3}
+          maxRows={10}
+          reviewText={reviewText}
+          onChange={handleChange}
+        />
+      </div>
+    </>
+  );
 }
