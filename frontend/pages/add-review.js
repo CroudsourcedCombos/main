0a4c1e81c933ef7300c90c3a56f4d0a0acb405e3
Raj Kumar <raj.a.kumar@ucla.edu>
2021-11-27 03:24:11 -0800

    Right shifted the Post button on the Add Review page and cleaned up CSS

--- frontend/pages/add-review.js
+++ frontend/pages/add-review.js
@@ -5,21 +5,22 @@ import styles from "../styles/Home.module.css";
 import TextField from "@mui/material/TextField";
 import Rating from "@mui/material/Rating";
 import ResponsiveAppBar from "../components/navbar";
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Button } from "@mui/material";
 
 export default function AddReview() {
   const [value, setValue] = React.useState(2);
@@ -31,44 +32,82 @@ export default function AddReview() {
 
   return (
     <>
-    
       <ResponsiveAppBar></ResponsiveAppBar>
-      <Card>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <Card style={{ width: "75%" }}>
+          <CardHeader
+            avatar={
+              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                R
+              </Avatar>
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title="Username"
+            subheader="Posting publicly"
+            paddingBottom="2px"
+          />
 
-        title="Username"
-        subheader="Posting publicly"
-      />
-      <CardContent>
-      {/* <div className={styles.container}>
-        <h1>Add Review</h1>
-      </div> */}
+          <CardContent style={{ paddingTop: "0px" }}>
+            <div>
+              <Rating
+                name="simple-controlled"
+                size="large"
+                // paddingTop="2px"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+            </div>
+            <div style={{paddingTop: '8px', paddingBottom: '8px'}}>
+              <TextField
+                id="filled-input-static"
+                label="Food Item"
+                // multiline
+                fullWidth
+                minRows={1}
+                maxRows={2}
+                reviewText={reviewText}
+                onChange={handleChange}
+                variant="filled"
+                paddingBottom="4px"
+              />
+            </div>
 
-
-      <div>
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Share rating details"
-          multiline
-          fullWidth
-          minRows={3}
-          maxRows={10}
-          reviewText={reviewText}
-          onChange={handleChange}
-        />
+            <div style={{paddingTop: '8px'}}>
+              <TextField
+                id="outlined-multiline-flexible"
+                label="Share rating details"
+                multiline
+                fullWidth
+                minRows={3}
+                maxRows={10}
+                reviewText={reviewText}
+                onChange={handleChange}
+                variant="filled"
+                // paddingBottom='8px'
+                // style={{padding: '10px 10px 10px 10px'}}
+              />
+            </div>
+          </CardContent>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              paddingRight: "10px",
+              paddingBottom: "16px",
+            }}
+          >
+            <Button size="small" color="primary">
+              Post
+            </Button>
+          </div>
+        </Card>
       </div>
-      </CardContent>
-      </Card>
     </>
   );
 }
