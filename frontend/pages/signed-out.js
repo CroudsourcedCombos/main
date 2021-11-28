e6aa76fdda92ab4e95a25c229ce48381d5296201
spatlol <siddharth.reddy7479@gmail.com>
2021-11-27 22:23:30 -0800

    implemented navbar component

---
+++ frontend/pages/signed-out.js
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import Image from "next/image";
+// import * as React from "react";
+import styles from "../styles/Home.module.css";
+import TextField from "@mui/material/TextField";
+import Rating from "@mui/material/Rating";
+import ResponsiveAppBar from "../components/navbar";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Button } from "@mui/material";
+import { useState } from "react";
+import CheckboxesGroup from "../components/foodCheckboxes";
+
+export default function AddReview() {
+  const [value, setValue] = useState(2);
+  const [reviewText, setreviewText] = useState("Controlled");
+
+  const handleChange = (event) => {
+    setreviewText(event.target.reviewText);
+  };
+
+
+  return (
+    <>
+      <ResponsiveAppBar></ResponsiveAppBar>
+    </>
+  );
+}
