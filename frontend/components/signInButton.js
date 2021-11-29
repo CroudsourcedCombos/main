7e2cb2cb2cef914b7a8a2e928bcab06fba09c6dd
SiddhantG02 <SiddhantG.me@gmail.com>
2021-11-29 15:32:08 -0800

    Added a sign in/up button component
    Component in components/signInButton can be used to create button to sign in or up for either facebook or google and accepts onClick events

--- frontend/components/signInButton.js
+++ frontend/components/signInButton.js
@@ -2,34 +2,32 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import TextField from "@mui/material/TextField";
 import ResponsiveAppBar from "../components/navbar";
-import SignInButton from "../components/signInButton"
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Button } from "@mui/material";
 import Head from 'next/head';
 import { Google,FacebookOutlined } from '@mui/icons-material';
-export default function SignUp() {
-  return (
-    <>
-      <ResponsiveAppBar />
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <Card>
-          <CardContent style={{ paddingTop: "50px",paddingBottom: "50px", justifyContent: 'center', width: "300px"}}>
-          <SignInButton type="google"  isSignIn="false"  onClick={()=>console.log('test')} />
-          <SignInButton type="facebook" isSignIn="false" />
-          </CardContent>
-        </Card>
-      </div>
-    </>
-  )
-}
+
+export default function SignInButton({ type, isSignIn, onClick }) {
+    if (type != 'facebook' && type != 'google')
+        return;
+    var isfacebook = false;
+    if(type == 'facebook'){
+        isfacebook = true;
+    }
+    return (
+      <Button style={{ width: '250px', justifyContent: "left" ,}} onClick={onClick} color ={isfacebook?"primary":"error"} variant="contained" startIcon={isfacebook?<FacebookOutlined/>:<Google/>}>
+        {isSignIn?"Sign in with":"Sign up with"} {isfacebook?"Facebook":"Google"}
+      </Button>
+    )
+  }
