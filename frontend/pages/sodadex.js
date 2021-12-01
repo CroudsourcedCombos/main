ac151485cf5da4e96d60654e1ca021f00a9ceaca
SiddhantG02 <SiddhantG.me@gmail.com>
2021-11-30 17:55:34 -0800

    Fixed component logic in sodadex grid
    Cleaned up interactions between review stars, hasTried field, and the comment box

--- frontend/pages/sodadex.js
+++ frontend/pages/sodadex.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -6,8 +6,8 @@ import TextField from "@mui/material/TextField";
 import Rating from "@mui/material/Rating";
 import ResponsiveAppBar from "../components/navbar";
 import { styled } from "@mui/material/styles";
-import { TableRow } from '@mui/material';
-import { DataGrid} from '@mui/x-data-grid';
+import { TableRow } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
@@ -23,104 +23,104 @@ import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Button } from "@mui/material";
-import { SODAS } from '../constants/soda';
+import { SODAS } from "../constants/soda";
 import { useAuth } from "../context/AuthenticatedUserContext";
 
 const renderAddReviewButton = ({ row, id }, rows, setRows) => {
   return (
-      <strong>
-          <Button
-              variant="contained"
-              color={row.ifUpdated?"primary":"error"}
-              size="small"
-              style={{ marginLeft: 16 }}
-              onClick={() => {
-                // Update the row update status
-                row.ifUpdated = true;
-                // Make a copy of the rows, update the row to what was passed in
-                //const rowCopy = [...rows];
-                //rowCopy[id] = row;
-                // Set the news state
-                //setRows(rowCopy);
-              }}
-          >
-              {row.ifUpdated?"Saved":"Update"}
-          </Button>
-      </strong>
-  )
-}
+    <strong>
+      <Button
+        variant="contained"
+        color={row.ifUpdated ? "primary" : "error"}
+        size="small"
+        style={{ marginLeft: 16 }}
+        onClick={() => {
+          // Update the row update status
+          row.ifUpdated = true;
+        }}
+      >
+        {row.ifUpdated ? "Saved" : "Update"}
+      </Button>
+    </strong>
+  );
+};
 const renderReviewStars = ({ row, id }, rows, setRows) => {
   // console.log('params :>> ', params);
   // const { row, id } = params
-return(
-  <Rating
-  name="simple-controlled"
-  size="medium"
-  value={row.rating}
-  onChange={(event) => {
-    // Update the row rating
-    //if()
-    row.rating = event.target.value;
-    row.ifUpdated = false;
-    row.hasTried = true;
-    row.hasTriedDisp = "Yes";
-    // Make a copy of the rows, update the row to what was passed in
-    const rowCopy = [...rows];
-    rowCopy[id] = row;
-
-    // Set the news state
-    setRows(rowCopy);
-  }}
-  />
-  )
-}
+  return (
+    <Rating
+      name="simple-controlled"
+      size="medium"
+      value={row.rating}
+      onChange={(event) => {
+        // Update the row rating
+        if (event.target.value != row.rating) {
+          row.rating = event.target.value;
+          row.hasTried = true;
+          row.hasTriedDisp = "Yes";
+        } else {
+          //If the same rating is clicked, we delete this review
+          row.rating = 0;
+          row.hasTried = false;
+          row.hasTriedDisp = "No";
+          row.reviewText = "";
+        }
+        row.ifUpdated = false;
+        // Make a copy of the rows, update the row to what was passed in
+        const rowCopy = [...rows];
+        rowCopy[id] = row;
+        // Set the news state
+        setRows(rowCopy);
+      }}
+    />
+  );
+};
 
 const renderReviewText = ({ row, id }, rows, setRows) => {
-  const [reviewText, setReviewText] = useState("Controlled");
-  return(
+  //const [reviewText, setReviewText] = useState("");
+  return (
     <TextField
       id="outlined-multiline-flexible"
       fullWidth
       multiline
       minRows={1}
       maxRows={3}
-      variant="filled"
-      reviewText={reviewText}
+      disabled={!row.hasTried}
       onChange={(event) => {
         // Update the row rating
-        setReviewText(event.target.reviewText);
-        row.ifUpdated = false;
-        row.hasTried = true;
-        row.hasTriedDisp = "Yes";
-        // Make a copy of the rows, update the row to what was passed in
-        //const rowCopy = {...rows};
-        //rowCopy[id] = row;
-        //setRows(rowCopy);
+        if (row.rating !== 0) {
+          row.reviewText = event.target.reviewText;
+        } else {
+          row.reviewText = "";
+        }
+        const rowCopy = [...rows];
+        rowCopy[id] = row;
+        // Set the new state
+        setRows(rowCopy);
+        //setReviewText(row.reviewText);
       }}
-  />
-  )
-}
-const drinks = ["Coke", "Fanta", "Sprite"];
+    />
+  );
+};
 
 function generateRowsFromSodas() {
   // Go through and change each hasTried to yes or no
-  let rows = SODAS.map(row => {
-    row.hasTriedDisp = row.hasTried ? 'Yes' : 'No'
-    return row
-  })
+  let rows = SODAS.map((row) => {
+    row.hasTriedDisp = row.hasTried ? "Yes" : "No";
+    return row;
+  });
 
   // Add state for each of the rating numbers, comments, and ifUpdated
-  rows = rows.map(row => {
+  rows = rows.map((row) => {
     // Set defaults for row for these properties
-    row.rating = '0'
-    row.comment = ''
-    row.ifUpdated = true
-    return row
-  })
+    row.rating = "0";
+    row.comment = "";
+    row.ifUpdated = true;
+    return row;
+  });
 
-  return rows
+  return rows;
 }
-
 
 export default function Sodadex() {
   // const [selectionModel, setSelectionModel] = useState([]);
@@ -135,60 +135,60 @@ export default function Sodadex() {
     else return "Joe Bruin";
   };
 
-  const [rows, setRows] = useState(generateRowsFromSodas)
+  const [rows, setRows] = useState(generateRowsFromSodas);
 
   const columns = [
-    { field: 'id', headerName: 'ID', width: 50 },
+    { field: "id", headerName: "ID", width: 50 },
     {
-      field: 'drink',
-      headerName: 'Soda Flavor',
+      field: "drink",
+      headerName: "Soda Flavor",
       width: 200,
       editable: false,
     },
     {
-      field: 'hasTriedDisp',
-      headerName: 'Have you tried it?',
+      field: "hasTriedDisp",
+      headerName: "Have you tried it?",
       width: 150,
       editable: false,
     },
     {
-      field: 'rating',
-      headerName: 'Your Rating',
+      field: "rating",
+      headerName: "Your Rating",
       width: 150,
-      renderCell: params => renderReviewStars(params, rows, setRows),
+      renderCell: (params) => renderReviewStars(params, rows, setRows),
       disableClickEventBubbling: true,
     },
     {
-      field: 'review',
-      headerName: 'Your comments',
+      field: "review",
+      headerName: "Your comments",
       width: 300,
-      renderCell: renderReviewText,
+      renderCell: (params) => renderReviewText(params, rows, setRows),
       disableClickEventBubbling: true,
     },
     {
-      field: 'postButton',
-      headerName: '',
+      field: "postButton",
+      headerName: "",
       width: 100,
-      renderCell: renderAddReviewButton,
+      renderCell: (params) => renderAddReviewButton(params, rows, setRows),
       disableClickEventBubbling: true,
     },
-  ]
+  ];
 
   function getHasTriedIndices() {
-    const indices = []
+    const indices = [];
     for (let x = 0; x < rows.length; x += 1) {
-      const row = rows[x]
-      if (row.hasTried) indices.push(x)
+      const row = rows[x];
+      if (row.hasTried) indices.push(x);
     }
-    return indices
+    return indices;
   }
 
   return (
     <>
       <ResponsiveAppBar />
-      <div style={{display: 'flex', justifyContent: 'center'}}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <Card style={{ width: "75%" }}>
-        <CardHeader
+          <CardHeader
             avatar={<Avatar alt={getUsername()} src={getProfilePicture()} />}
             action={
               <IconButton aria-label="settings">
@@ -201,15 +201,20 @@ export default function Sodadex() {
           />
 
           <CardContent style={{ paddingTop: "0px" }}>
-            <div>
+            <div></div>
+            <div
+              style={{
+                paddingTop: "8px",
+                paddingBottom: "8px",
+                justifyContent: "center",
+              }}
+            >
+              <header>
+                <h1>The Sodadex</h1>
+                <p>Explore new flavors. Track your favorite combos!</p>
+              </header>
             </div>
-            <div style={{paddingTop: '8px', paddingBottom: '8px', justifyContent: 'center'}}>
-            <header>
-              <h1 >The Sodadex</h1>
-              <p>Explore new flavors. Track your favorite combos!</p>
-            </header>
-            </div>
-            <div style={{ height: 400, width: '100%' }}>
+            <div style={{ height: 400, width: "100%" }}>
               <DataGrid
                 rows={rows}
                 columns={columns}
@@ -219,16 +224,14 @@ export default function Sodadex() {
                 checkboxSelection
                 selectionModel={getHasTriedIndices()}
                 hideFooterSelectedRowCount
-                onSelectionModelChange={
-                  (ids) => {
-                    console.log(ids)
-                    for (const id of ids) {
-                      rows[id].hasTried = true;
-                      rows[id].hasTriedDisp = 'Yes';
-                    }
-                    setRows(rows);
+                onSelectionModelChange={(ids) => {
+                  console.log(ids);
+                  for (const id of ids) {
+                    rows[id].hasTried = true;
+                    rows[id].hasTriedDisp = "Yes";
                   }
-                }
+                  setRows(rows);
+                }}
               />
             </div>
           </CardContent>
@@ -239,8 +242,7 @@ export default function Sodadex() {
               paddingRight: "10px",
               paddingBottom: "16px",
             }}
-          >
-          </div>
+          ></div>
         </Card>
       </div>
     </>
