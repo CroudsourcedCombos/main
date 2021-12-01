61c35b29210ffea53afa42cf4f55f1b843255b0d
spatlol <siddharth.reddy7479@gmail.com>
2021-12-01 00:54:51 -0800

    copied over correct navbar from main

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -13,15 +13,39 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 // import {NavLink} from "react-router-dom";
 // import {Link} from "react-router-dom";
-import Link from "@material-ui/core";
+import Link from "@material-ui/core/Link";
+import { useAuth } from "../context/AuthenticatedUserContext";
+import { useRouter } from "next/router";
 
-const pages = ["Add Review", "Flavors", "Menu", "Sign In/Up"];
-const pageRoutes = ["add-review", "flavors", "menu", "sign-in"]
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+import { _signOut } from "./auth/signOut";
 
-const ResponsiveAppBar = () => {
+// const settings = ["Logout"]; // "Profile", "Account", "Dashboard",
+const PAGES = ["Add Review", "Soda", "Menu", "Sign In/Up"];
+const PAGE_ROUTES = ["add-review", "sodadex", "menu", "sign-in"];
+
+const styles = {
+  navbarLink: {
+    color: "white",
+    textDecorationColor: "white",
+  },
+};
+
+const NavigationBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const { user, setUser } = useAuth();
+  const router = useRouter();
+
+  // We want settings to dynamically update if signed in or not
+
+  const settings = [user ? "Sign Out" : "Sign In"];
+
+  // If the user is signed in, then no sign in/up option
+  // TO-DO: Refactor this with useRef?
+  const pages = user ? PAGES.slice(0, PAGES.length - 1) : PAGES;
+  const pageRoutes = user
+    ? PAGE_ROUTES.slice(0, PAGE_ROUTES.length - 1)
+    : PAGE_ROUTES;
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -31,6 +55,23 @@ const ResponsiveAppBar = () => {
     setAnchorElNav(null);
   };
 
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const getProfilePicture = () => {
+    if (user) return user.photoURL;
+    else return "/static/images/avatar/2.jpg";
+  };
+
+  const getUsername = () => {
+    if (user) return user.displayName;
+    else return "Joe Bruin";
+  };
 
   return (
     <AppBar position="static">
@@ -42,69 +83,76 @@ const ResponsiveAppBar = () => {
             component="div"
             sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
           >
-            Crowdsourced Combos
+            <Link style={styles.navbarLink} href="/">
+              Crowdsourced Combos
+            </Link>
           </Typography>
-
 
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, display: { xs: "flex", md: "none"} }}
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
           >
             LOGO
           </Typography>
-          
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" }, justifyContent: "right"}}>
-            {pages.map((page, index) => (
-                <Button
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                  // component={Link}
-                  // to="/sign-in"
-                  key={page}
 
-                  href={pageRoutes[index]}
-                >
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "right",
+            }}
+          >
+            {pages.map((page, index) => (
+              <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+                // component={Link}
+                // to="/sign-in"
+                key={page}
+              >
+                <Link style={styles.navbarLink} href={pageRoutes[index]}>
                   {page}
-                </Button>
+                </Link>
+              </Button>
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }}>
-          <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt={getUsername()} src={getProfilePicture()} />
+              </IconButton>
+            </Tooltip>
             <Menu
+              sx={{ mt: "45px" }}
               id="menu-appbar"
-              anchorEl={anchorElNav}
+              anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
                 vertical: "top",
-                horizontal: "left",
+                horizontal: "right",
               }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
             >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center" justifyContent="right">
-                    {page}
-                  </Typography>
+              {settings.map((setting) => (
+                <MenuItem
+                  key={setting}
+                  onClick={() => {
+                    handleCloseNavMenu();
+
+                    // Choose the appropriate action based on if signing in or out
+                    if (setting === "Sign Out") _signOut();
+                    else router.push("/sign-in");
+                  }}
+                >
+                  <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
               ))}
             </Menu>
@@ -114,4 +162,4 @@ const ResponsiveAppBar = () => {
     </AppBar>
   );
 };
-export default ResponsiveAppBar;
+export default NavigationBar;
