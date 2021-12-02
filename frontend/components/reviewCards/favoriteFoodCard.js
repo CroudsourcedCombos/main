9e8bb0dcbd8d917ad95624c9153b2579c0d5cdf1
SiddhantG02 <SiddhantG.me@gmail.com>
2021-12-01 17:54:07 -0800

    Added a my-favorites page

--- frontend/components/reviewCards/favoriteFoodCard.js
+++ frontend/components/reviewCards/favoriteFoodCard.js
@@ -2,12 +2,10 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import {Container} from "@mui/material";
+import { Container } from "@mui/material";
 import Rating from "@mui/material/Rating";
-
-export default function MyFoodReviewCard({score, category, name, text}) {
-  const ingredients = JSON.parse(name)
-
+import Button from "@mui/material/Button";
+export default function FavoriteFoodCard({ score, category, ingredients }) {
   // Format ingredients to be all strings
   function formatIngredient(ingredients) {
     // If it's an array, format it
@@ -16,17 +14,13 @@ export default function MyFoodReviewCard({score, category, name, text}) {
     else return titleCase(ingredients);
   }
 
-  // Title case any strings
-  // TO-DO: Move this to be shared across multiple components
   function titleCase(str) {
-    if (str === "") return "";
     const words = str.split(/\s+/);
     const titleCaseArr = words.map(
       (substr) => substr[0].toUpperCase() + substr.slice(1)
     );
     return titleCaseArr.join(" ");
   }
-
   return (
     <Card
       sx={{
@@ -37,7 +31,7 @@ export default function MyFoodReviewCard({score, category, name, text}) {
         flexDirection: "column",
       }}
     >
-      <Container sx={{width: "100%", paddingTop: "20px"}}>
+      <Container sx={{ width: "100%", paddingTop: "20px" }}>
         <CardContent
           sx={{
             display: "flex",
@@ -60,12 +54,12 @@ export default function MyFoodReviewCard({score, category, name, text}) {
             }}
           >
             <Typography
-              sx={{fontSize: 11, paddingRight: "15px"}}
+              sx={{ fontSize: 15, paddingRight: "15px" }}
               color="text.secondary"
             >
               {"Rating: "}
             </Typography>
-            <Rating value={score} readOnly size="medium"/>
+            <Rating value={score} readOnly size="medium" />
           </CardContent>
         </CardContent>
         <CardContent
@@ -74,40 +68,29 @@ export default function MyFoodReviewCard({score, category, name, text}) {
             justifyContent: "space-between",
             flexFlow: "row wrap",
             "&:last-child": {
-              paddingBottom: 0,
               paddingTop: 0,
             },
           }}
         >
           {Object.keys(ingredients).map((key) => (
-            <Typography sx={{fontSize: 12}} color="text.primary" gutterBottom>
+            <Typography sx={{ fontSize: 12 }} color="text.primary" gutterBottom>
               {titleCase(key)}:{" "}
               <strong>{formatIngredient(ingredients[key])}</strong>
             </Typography>
           ))}
         </CardContent>
+        {/* <Button
+          onClick={() => {
+            toggleFav();
+          }}
+        >
+          {IsLiked ? (
+            <FavoriteIcon></FavoriteIcon>
+          ) : (
+            <FavoriteBorderIcon></FavoriteBorderIcon>
+          )}
+        </Button> */}
       </Container>
-      {
-        text !== "" ? <Container sx={{width: "100%"}}>
-          <CardContent>
-            <Typography
-              sx={{
-                fontSize: 14,
-                "&:last-child": {
-                  paddingBottom: 0,
-                },
-              }}
-              color="text.secondary"
-              gutterBottom
-            >
-              Comments
-            </Typography>
-            <Typography variant="h7" color="text.primary" gutterBottom>
-              {text}
-            </Typography>
-          </CardContent>
-        </Container> : <></>
-      }
     </Card>
   );
 }
---
+++ frontend/pages/my-favorites.js
@@ -0,0 +1,117 @@
+import ResponsiveAppBar from "../components/navbar";
+import { useState } from "react";
+// import * as React from "react";
+import CardHeader from "@mui/material/CardHeader";
+import Avatar from "@mui/material/Avatar";
+import { Container } from "@mui/material";
+import FavoriteFoodCard from "../components/reviewCards/favoriteFoodCard";
+import FavoriteSodaCard from "../components/reviewCards/favoriteSodaCard";
+const foods = [
+  {
+    score: 10,
+    category: "Sandwich",
+    ingredients: {
+      bread: "sourdough",
+      cheese: ["provolone"],
+      toppings: ["tomato", "lettuce"],
+      sauce: ["mayo", "sundried tomato pesto"],
+    },
+  },
+  {
+    score: 2,
+    category: "Sub",
+    ingredients: {
+      bread: "torpedo hoagie",
+      cheese: ["swiss"],
+      toppings: ["onion"],
+      sauce: ["sundried tomato pesto"],
+    },
+  },
+  {
+    score: 7,
+    category: "Salad",
+    ingredients: {
+      greens: "baby spinach",
+      protein: "chicken",
+      toppings: ["goat cheese", "olives", "garbanzo beans"],
+      dressing: "ranch",
+    },
+  },
+  {
+    score: 4,
+    category: "Coffee",
+    ingredients: {
+      type: "french press",
+      milk: "none",
+      sugar: "2 packets",
+    },
+  },
+];
+
+const drinks = [
+  {
+    score: 2,
+    category: "Freestyle Drinks",
+    id: 1,
+    comment: "cant beat original",
+  },
+  {
+    score: 3.5,
+    category: "Freestyle Drinks",
+    id: 2,
+    comment: "coolio",
+  },
+  {
+    score: 4,
+    category: "Freestyle Drinks",
+    id: 3,
+    comment: "hype",
+  },
+];
+
+export default function myReviews({ user }) {
+  const [value, setValue] = useState(2);
+  const [reviewText, setreviewText] = useState("Controlled");
+  const handleChange = (event) => {
+    setreviewText(event.target.reviewText);
+  };
+  const getProfilePicture = () => {
+    if (user) return user.photoURL;
+    else return "/static/images/avatar/2.jpg";
+  };
+
+  const getUsername = () => {
+    if (user) return user.displayName;
+    else return "Joe Bruin";
+  };
+
+  return (
+    <>
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <CardHeader
+        avatar={<Avatar alt={getUsername()} src={getProfilePicture()} />}
+        display="flex"
+        justifyContent="center"
+        title="My Favorites:"
+        paddingBottom="2px"
+      />
+      <Container
+        maxWidth="xl"
+        sx={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <Container sx={{ width: "50%", margin: "10px" }}>
+          <FavoriteFoodCard {...foods[0]} />
+          <FavoriteFoodCard {...foods[1]} />
+          <FavoriteFoodCard {...foods[2]} />
+        </Container>
+        <Container sx={{ width: "50%", margin: "10px" }}>
+          <FavoriteSodaCard {...drinks[0]} />
+          <FavoriteSodaCard {...drinks[1]} />
+          <FavoriteSodaCard {...drinks[0]} />
+          <FavoriteSodaCard {...drinks[1]} />
+          <FavoriteSodaCard {...drinks[0]} />
+        </Container>
+      </Container>
+    </>
+  );
+}
